0db88d6bc661d18dac195247f3065f8662dbba5c
Mehedyh259 <mehedyh259@gmail.com>
2022-05-21 19:44:03 +0600

    axios interceptor added

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import fetcher from '../api';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+
+    useEffect(() => {
+        (async () => {
+            const res = await fetcher.get("/service");
+            setServices(res.data)
+
+        })()
+    }, [])
+
+    console.log(services);
+
     return (
         <div className='mt-16'>
             <h2>this is services</h2>
